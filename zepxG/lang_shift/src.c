8d172a62f99bc6d0f1d8b775f91ddd86f9704727
Alex <alex.fnpimnr@gmail.com>
2025-08-08 18:03:16 -0300

    1

--- zepxG/lang_shift/src.c
+++ zepxG/lang_shift/src.c
@@ -560,13 +560,13 @@ bool lang_shift_process_custom_keycodes(Key key, keyrecord_t* record) {
     case GO_TO_RU:
        if (down) {                  // Действуем только при нажатии
         lang_activate_from_user(0); // 1. Устанавливаем целевой язык - русский (0)
-        layer_move(0);              // 2. Принудительно переключаем клавиатуру на слой 0
+        layer_on(0);              // 2. Принудительно переключаем клавиатуру на слой 0
       }
       return false; 
     case GO_TO_EN:
        if (down) {                  // Действуем только при нажатии
-        lang_activate_from_user(0); // 1. Устанавливаем целевой язык - английский (1)
-        layer_move(0);              // 2. Принудительно переключаем клавиатуру на слой 1
+        lang_activate_from_user(1); // 1. Устанавливаем целевой язык - английский (1)
+        layer_on(1);              // 2. Принудительно переключаем клавиатуру на слой 1
       }
       return false;   
     case AG_3DOT:
