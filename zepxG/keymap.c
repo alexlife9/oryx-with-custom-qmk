772ce35d5fe0e6004dbcaba37a254d601261f8ee
Alex <alex.fnpimnr@gmail.com>
2025-08-12 20:54:06 -0300

    1

--- zepxG/keymap.c
+++ zepxG/keymap.c
@@ -164,7 +164,9 @@ bool rgb_matrix_indicators_user(void) {
 }
 
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+#include "custom_logic.c"                                            // это первое добавление
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {    // это оригинальная строка
+    if (!process_record_custom(keycode, record)) { return false; }   // это второе добавление
   switch (keycode) {
     case ST_MACRO_0:
     if (record->event.pressed) {
