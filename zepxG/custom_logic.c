305046506c97f5e6cec4a6f5d32b21feb33f2014
Alex <alex.fnpimnr@gmail.com>
2025-08-30 01:44:26 -0300

    1

--- zepxG/custom_logic.c
+++ zepxG/custom_logic.c
@@ -196,14 +196,17 @@ bool process_record_custom(uint16_t keycode, keyrecord_t *record) {
                     // ДА, это двойное нажатие.
                     // "Исправляем" предыдущее действие.
                     tap_code(KC_BSPC);                // 1. Стираем <
-                    SEND_STRING("<!---->" SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT)); // 2. Печатаем <!-- --> и ставим курсор внутрь
+                    SEND_STRING((SS_LALT(SS_TAP(X_KP_6) SS_TAP(X_KP_0) SS_TAP(X_LEFT_ALT) ))
+                                "!----"
+                                (SS_LALT(SS_TAP(X_KP_6) SS_TAP(X_KP_2) SS_TAP(X_LEFT_ALT) ))
+                                SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT)); // 2. Печатаем <!-- --> и ставим курсор внутрь
 
                     // Сбрасываем таймер, чтобы последовательность не продолжилась.
                     labk_timer = 0;
                 } else {
                     // НЕТ, это одиночное нажатие.
                     // Действуем немедленно.
-                    tap_code16(S(KC_COMMA)); // Печатаем '<'
+                    SEND_STRING(SS_LALT(SS_TAP(X_KP_6) SS_TAP(X_KP_0) SS_TAP(X_LEFT_ALT) )); // Печатаем '<'
 
                     // Запускаем таймер, чтобы отследить возможное второе нажатие.
                     labk_timer = timer_read();
